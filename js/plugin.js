7d79352df6bcb75ca86ed3810063e9ea3e4bb0bc
Omar-Abdulkhalek <omarabdelkhalek760@gmail.com>
2021-05-03 17:20:49 +0200

    Intilization 7

--- js/plugin.js
+++ js/plugin.js
@@ -1,7 +1,15 @@
-VanillaTilt.init(document.querySelector(".container"), {
-    max: 5,
-    speed: 200,
-    glare:true,
-    "max-glare":.2,
 
-});
+
+    
+    VanillaTilt.init(document.querySelector(".container"), {
+        max: 5,
+        speed: 200,
+        glare: true,
+        "max-glare": .2,
+
+    });
+
+
+
+
+
